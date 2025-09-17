4e1303b4db33eaba196475f9a269308b05033885
Samridhi Chaudhary <samridhi.chaudhary16@gmail.com>
2025-09-17 15:12:36 +0530

    mobile responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,32 @@
+import React, { useState } from "react";
 import "./Header.css";
 import { Search, ShoppingCart, User } from "lucide-react";
-import { Link } from "react-router-dom";
 
 function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <header className="header">
       <div className="logo">ShopFlow</div>
 
-      <nav className="nav-links">
-        <Link to="/">Home</Link>
-        <Link to="/products">Products</Link>
+      {/* Hamburger Button */}
+      <button className="hamburger" onClick={toggleMenu}>
+        ☰
+      </button>
+
+      {/* Navigation Links */}
+      <nav className={`nav-links ${menuOpen ? "open" : ""}`}>
+        <a href="#">Home</a>
+        <a href="#">Products</a>
         <a href="#">Categories</a>
         <a href="#">About</a>
       </nav>
+
+      {/* Icons (Hidden on mobile) */}
       <div className="icons">
         <Search className="icon" />
         <User className="icon" />
